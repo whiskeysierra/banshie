412c0a3a1bfff03941fe741e8905321ee9cdb637
Willi Schönborn <w.schoenborn@gmail.com>
2012-07-10 13:15:46 +0200

    updated poms

--- jarvis-common/src/main/java/org/whiskeysierra/jarvis/Activator.java
+++ jarvis-common/src/main/java/org/whiskeysierra/jarvis/Activator.java
@@ -5,10 +5,12 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+    @Override
     public void start(BundleContext context) throws Exception {
         // TODO add activation code here
     }
 
+    @Override
     public void stop(BundleContext context) throws Exception {
         // TODO add deactivation code here
     }
