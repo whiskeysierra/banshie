75bdad412a16da50a9c13247757b78ba7ea625be
Willi Schönborn <w.schoenborn@gmail.com>
2013-01-22 17:10:47 +0100

    split up banshie-execution into different modules

--- banshie-execution/src/main/java/org/whiskeysierra/banshie/execution/ExecutionModule.java
+++ banshie-execution/src/main/java/org/whiskeysierra/banshie/execution/ExecutionModule.java
@@ -1,8 +1,6 @@
 package org.whiskeysierra.banshie.execution;
 
 import com.google.inject.AbstractModule;
-import org.whiskeysierra.banshie.execution.event.EventModule;
-import org.whiskeysierra.banshie.execution.process.ProcessModule;
 
 import static org.ops4j.peaberry.Peaberry.service;
 import static org.ops4j.peaberry.util.TypeLiterals.export;
@@ -11,9 +9,6 @@ public final class ExecutionModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        install(new EventModule());
-        install(new ProcessModule());
-
         bind(export(Engine.class)).toProvider(service(DefaultEngine.class).export());
     }
 
