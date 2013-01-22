96394bd8688443e205cced06b6bd83c14a288211
Willi Schönborn <w.schoenborn@gmail.com>
2013-01-22 22:54:21 +0100

    added binding statement to Engine

--- banshie-execution/src/main/java/org/whiskeysierra/banshie/execution/ExecutionModule.java
+++ banshie-execution/src/main/java/org/whiskeysierra/banshie/execution/ExecutionModule.java
@@ -1,6 +1,7 @@
 package org.whiskeysierra.banshie.execution;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 
 import static org.ops4j.peaberry.Peaberry.service;
 import static org.ops4j.peaberry.util.TypeLiterals.export;
@@ -9,6 +10,7 @@ public final class ExecutionModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bind(Engine.class).to(DefaultEngine.class).in(Singleton.class);
         bind(export(Engine.class)).toProvider(service(DefaultEngine.class).export());
     }
 
