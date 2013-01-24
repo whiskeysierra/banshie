9c71d7cc0cb552473041ca3e78f26f2ff87723f8
Willi Schönborn <w.schoenborn@gmail.com>
2013-01-24 18:24:53 +0100

    removed comment

--- banshie-persistence/src/main/java/org/whiskeysierra/banshie/persistence/PersistenceModule.java
+++ banshie-persistence/src/main/java/org/whiskeysierra/banshie/persistence/PersistenceModule.java
@@ -3,13 +3,11 @@ package org.whiskeysierra.banshie.persistence;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import static org.ops4j.peaberry.Peaberry.service;
 import static org.ops4j.peaberry.util.Attributes.names;
 import static org.ops4j.peaberry.util.TypeLiterals.export;
-import static org.ops4j.peaberry.util.Filters.ldap;
 
 public final class PersistenceModule extends AbstractModule {
 
@@ -19,11 +17,6 @@ public final class PersistenceModule extends AbstractModule {
 
         bind(export(DataSource.class)).toProvider(
             service(DataSource.class).attributes(names("osgi.jndi.service.name=jdbc/banshiedb")).export());
-
-        /*
-        bind(EntityManagerFactory.class).toProvider(
-            service(EntityManagerFactory.class).filter(ldap("(osgi.unit.name=banshie)")).single()).in(Singleton.class);
-            */
     }
 
 }
